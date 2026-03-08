6fcae8199a3f45f3a1084765de98f26b015176f6
gkrayeski <gkrayeski@frba.utn.edu.ar>
2026-03-08 00:32:31 -0300

    structure corrections

---
+++ src/steps/feature_engineering/invest_feature_pipe/steps_pi.py
@@ -0,0 +1,52 @@
+from steps.feature_engineering.I_featuring import IFeaturing
+import pandas_ta as ta
+
+class Ema_aggregation(IFeaturing):
+
+    def __init__(self, periods):
+        self.periods = periods
+
+    def process(self, data):
+        data[f'ema{str(self.periods)}'] = data['adjusted_clodse'].ewm(span=self.periods,adjust=False).mean()
+        data = self._get_distance(data)
+        data.drop(f'ema{str(self.periods)}',axis=1, inplace=True)
+        return data
+    
+    def _get_distance(self, data):
+        data[f'distance_ema_{str(self.periods)}'] = (data[f'ema{str(self.periods)}'] - data['adjusted_close']) / data[f'ema{str(self.periods)}']
+    
+    
+
+class Mma_agregation(IFeaturing):
+
+    def __init__(self, periods):
+        self.periods = periods
+
+    def process(self, data):
+        data[f'mma{str(self.periods)}'] = data['adjusted_close'].rolling(self.periods).mean()
+        data = self._get_distance(data)
+        data.drop(f'mma{str(self.periods)}',axis=1, inplace=True)
+        return data
+
+    def _get_distance(self, data):
+        data[f'distance_mma_{str(self.periods)}'] = (data[f'mma{str(self.periods)}'] - data['adjusted_close']) / data[f'mma{str(self.periods)}']
+        
+
+class Rsi_aggregation(IFeaturing):
+
+    def __init__(self, periods):
+        self.periods = periods
+    
+    def process(self, data):
+        data[f'rsi_{str(self.periods)}'] = ta.rsi(data['adjusted_close'], length= int(self.periods))
+        return data
+
+
+class Target_agregation(IFeaturing):
+
+    def __init__(self, periods):
+        self.periods = periods
+        
+    def process(self, data):
+        data['adjusted_close'].shift( - int(self.periods)).pct_change()
+        return data
