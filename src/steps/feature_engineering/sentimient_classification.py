86272ea8d5a8285019e452cf07fa89ad3414c89c
gkrayeski <gkrayeski@frba.utn.edu.ar>
2026-03-14 14:19:35 -0300

    finished minimizing img and performance with batches

--- src/steps/feature_engineering/sentimient_classification.py
+++ src/steps/feature_engineering/sentimient_classification.py
@@ -9,19 +9,32 @@ class SentimentClassification(IFeaturing):
         self.nlp = pipeline("sentiment-analysis", model="ProsusAI/finbert")
 
     def process(self, data):
-        final_df = data.copy()
-        final_df["news_score"] = final_df.apply(
-            lambda row: self._get_sentiment(row['real_news'], row['ma']), 
-            axis=1
-        )
-        return final_df
+        process_data = data.copy()
+        return self._get_sentiment(process_data)
 
-    def _get_sentiment(self, text, ma_label):
-        if not text or pd.isna(text):
-            return 0.0
-        result = self.nlp(self._contextualize_model(text[:512]))
-        ai_label = result[0]['label'].lower()
-        ai_score = result[0]['score']
+    def _get_sentiment(self, data):
+        contextualized_texts = [
+            self._contextualize_model(str(txt)[:2000]) if pd.notna(txt) and txt else ""
+            for txt in data["real_news"]
+        ]
+        score_result = self.nlp(contextualized_texts, batch_size=100)
+        scores_finales = []
+        for i, result in enumerate(score_result):
+            noticia_original = data.iloc[i]['real_news']
+            if not noticia_original or pd.isna(noticia_original):
+                scores_finales.append(0.0)
+            else:
+                ai_label = result['label'].lower()
+                ai_score = result['score']
+                ma_val = data.iloc[i]["ma"]
+                scores_finales.append(
+                    self._calcular_score_final(ai_label,ai_score,ma_val)
+                )
+        data["news_scores"] = scores_finales
+        return data
+
+
+    def _calcular_score_final(self, ai_label, ai_score, ma_label):        
         if ma_label == 0:
             if ai_label == 'negative':
                 return ai_score * -1
